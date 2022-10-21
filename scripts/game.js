fb7daa4aad188160849d00a5889f18b0eccff1a1
unkhan87 <ukhan87@icloud.com>
2022-10-21 18:26:10 +0000

    More Advance Tests Part 1

--- scripts/game.js
+++ scripts/game.js
@@ -5,5 +5,23 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"]
 };
 
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addturn();
+}
 
-module.exports = { game };
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.randon() * 4))]);
+    //showTurns();
+}
+
+function showScore() {
+    document.getElementById("score").innerText=game.score;
+}
+
+
+module.exports = { game, newGame, showScore };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
- const { game } = require("../game");
+ const { game, newGame, showScore } = require("../game");
 
 
  beforeAll(() => {
@@ -29,4 +29,26 @@
      test("choices contain correct ids", () => {
          expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
      });
+ });
+
+ describe("newGame works correctly", () => {
+    beforeAll (() => {
+        game.score = 42;
+        game.playerMoves = ["button", "button2"];
+        game.currentGame = ["button", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should be one move in the computer's game array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should display 0 for the element with if of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    });
  });
